b6d0ea62d591ed9302aca76994b769818985d406
jojisagi <jorgesanchez.js601@gmail.com>
2025-11-04 16:04:04 -0600

    Push intento buenas practicas Hector

--- app.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-from view.simple_view import main
-
-if __name__ == '__main__':
-    ft.app(target=main)
--- controller/cliente_controller.py
+++
@@ -1,9 +0,0 @@
-from model.repositories import ClienteModelo
-
-class ClienteControlador:
-    def __init__(self, modelo: ClienteModelo):
-        self.m = modelo
-
-    def insertar_y_verificar(self, *args, **kwargs):
-        # Mantener posicionales/keywords intactos
-        return self.m.insertar_y_verificar(*args, **kwargs)
